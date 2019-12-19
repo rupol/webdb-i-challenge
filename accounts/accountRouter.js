222ff9cdbaa305499661d42f73f82df1bc792845
Ruth Poliakon <r.poliakon@gmail.com>
2019-12-18 18:17:01 -0700

    GET account endpoint

--- accounts/accountRouter.js
+++ accounts/accountRouter.js
@@ -3,11 +3,26 @@ const db = require("../data/dbConfig"); // database access using knex
 
 const router = express.Router();
 
+///// GET ACCOUNTS /////
 router.get("/", async (req, res, next) => {
   db("accounts")
     .select()
     .then(accounts => {
       res.json(accounts);
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+///// GET ACCOUNT /////
+router.get("/:id", async (req, res, next) => {
+  db("accounts")
+    .select()
+    .where("id", req.params.id)
+    .first()
+    .then(account => {
+      res.json(account);
     })
     .catch(error => {
       next(error);
